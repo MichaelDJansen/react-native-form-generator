a9ff5987bb793de04c495a485e41cfe273a89b2f
Freddie <freddieodonnell@afrozaar.com>
2017-04-18 16:57:30 +0200

    adding old date value to onChange method

--- src/lib/DatePickerComponent.android.js
+++ src/lib/DatePickerComponent.android.js
@@ -27,7 +27,8 @@ import {Field} from './Field';
 
       if ((this.props.noDateChange == undefined || this.props.noDateChange == null) || !this.props.noDateChange) this.setState({date:date});
 
-      if(this.props.onChange)      this.props.onChange((this.props.prettyPrint)?this.props.dateTimeFormat(date):date);
+      if(this.props.onChange)      this.props.onChange((this.props.prettyPrint)?this.props.dateTimeFormat(date):date,
+                                                          (this.props.prettyPrint)?this.props.dateTimeFormat(this.props.date):this.props.date);
       if(this.props.onValueChange) this.props.onValueChange(date, this.props.date);
     }
     setDate(date){
